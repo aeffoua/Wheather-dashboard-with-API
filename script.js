0a5a7ccf9a358416cf6b60c7cc2e8513a08ec1e3
A Effoua <aeffoua@As-MacBook-Air.attlocal.net>
2023-02-09 18:31:45 -0500

    added localstorage to js

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-var APIKey= 'c317fe562c65e62a24be96830a417e95';
-var searchBtn= document.getElementById("searchBtn");
-function searchCity(){
-var nameOfCity= document.getElementById('form1').value
-console.log(nameOfCity);
-fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + nameOfCity + '&appid=c317fe562c65e62a24be96830a417e95')
-.then((response) => response.json())
-.then((data) =>{
-   showWeather(data)
-// fetch('api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=c317fe562c65e62a24be96830a417e95')
-});
-
-}
+var APIKey = 'c317fe562c65e62a24be96830a417e95';
+var searchBtn = document.getElementById("searchBtn");
 searchBtn.addEventListener("click", searchCity);
 
-function mainWeather(payload){
-    let template= `
+getHistory()
+
+function searchCity() {
+    var nameOfCity = document.getElementById('form1').value
+    console.log(nameOfCity);
+    fetchData(nameOfCity)
+}
+
+function fetchData(nameOfCity) {
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + nameOfCity + '&appid=c317fe562c65e62a24be96830a417e95')
+        .then((response) => response.json())
+        .then((data) => {
+            showWeather(data)
+            // fetch('api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=c317fe562c65e62a24be96830a417e95')
+        });
+}
+
+function mainWeather(payload) {
+    let template = `
     <div class="border border-secondary p-2 mt-2">
                     <div class="fw-bold fs-3 text-left"> ${payload.city} (${payload.date}) <img src="http://openweathermap.org/img/w/${payload.icon}.png"/></div>
                     <div class="mt-2">Temp: ${payload.temp} F</div>
@@ -25,37 +31,45 @@ function mainWeather(payload){
                 `
     return template
 }
-function showWeather(data){
-    if(data.cod!=200){
-        return 
+
+
+function showWeather(data) {
+    if (data.cod != 200) {
+        alert('This City does not exist')
+        return
     }
-    document.getElementById('mainsection').innerHTML=''
-    document.getElementById('dailybox').innerHTML=''
-    for(let i=0;data.list.length>i;i+=8){
+    document.getElementById('mainsection').innerHTML = ''
+    document.getElementById('dailybox').innerHTML = ''
+    let city = data.city.name
+    saveCity(city)
+    getHistory(city)
+    for (let i = 0; data.list.length > i; i += 8) {
         console.log(data.list[i])
-        let payload={
-            city:data.city.name,
-            temp:data.list[i].main.temp,
-            icon:data.list[i].weather[0].icon,
-            wind:data.list[i]. wind.speed,
-            humidity:data.list[i].main.humidity,
-            date:dayjs.unix(data.list[i].dt).format('MM/DD/YYYY')
+        let payload = {
+            city: data.city.name,
+            temp: data.list[i].main.temp,
+            icon: data.list[i].weather[0].icon,
+            wind: data.list[i].wind.speed,
+            humidity: data.list[i].main.humidity,
+            date: dayjs.unix(data.list[i].dt).format('MM/DD/YYYY')
         }
-        if(i==0){
-        let mainSection=mainWeather(payload)
-            document.getElementById('mainsection').innerHTML=mainSection
-        
+        if (i == 0) {
+            let mainSection = mainWeather(payload)
+            document.getElementById('mainsection').innerHTML = mainSection
+
         }
-        let daily= document.createElement('div')
+        let daily = document.createElement('div')
         daily.classList.add('border', 'text-light', 'weather-card')
-        daily.innerHTML=dayWeather(payload)
-       
+        daily.innerHTML = dayWeather(payload)
+
         document.getElementById('dailybox').appendChild(daily)
     }
 
 }
-function dayWeather(payload){
-    let template=`
+
+
+function dayWeather(payload) {
+    let template = `
     <div> Date: ${payload.date}</div>
     <div> <img src="http://openweathermap.org/img/w/${payload.icon}.png"/></div>
     <div> Temp: ${payload.temp} F</div>
@@ -65,3 +79,43 @@ function dayWeather(payload){
     `
     return template
 }
+
+function saveCity(city) {
+    let storagedata = Object.values(window.localStorage.valueOf())
+    if (storagedata.includes(city)) {
+        return
+    }
+    let key = window.localStorage.length
+    window.localStorage.setItem(key, city)
+
+}
+
+
+function showHistory(el) {
+    let historyDiv = document.getElementById('history')
+    let div = document.createElement('div')
+    div.classList.add('d-grid', 'mt-3', 'gap-2')
+    let button = document.createElement('button')
+    button.classList.add('btn', 'form1', 'btn-secondary')
+    button.innerHTML = el
+    div.appendChild(button)
+    button.addEventListener("click", clickHistory);
+    historyDiv.appendChild(div)
+}
+
+
+function getHistory() {
+    let historyDiv = document.getElementById('history')
+    historyDiv.innerHTML = ''
+    let history = Object.values(window.localStorage.valueOf())
+    for (let el of history) {
+        showHistory(el)
+    }
+}
+
+
+function clickHistory(e) {
+    let nameOfCity = e.target.textContent
+    fetchData(nameOfCity)
+}
+
