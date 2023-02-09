c10777038e334f834f11135b5c0d35c48d137e90
A Effoua <aeffoua@As-MacBook-Air.attlocal.net>
2023-02-08 22:07:53 -0500

    added acceptance criteria to readme

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Wheather-dashboard-with-API
 
-![Screenshot 2023-02-08 at 9 33 01 PM (2)](https://user-images.githubusercontent.com/119564199/217704940-d6882f55-6ce8-4807-bad0-ea837bbd8b50.png)
+
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+
+
+
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+
--- script.js
+++ script.js
@@ -1,16 +1,67 @@
 var APIKey= 'c317fe562c65e62a24be96830a417e95';
 var searchBtn= document.getElementById("searchBtn");
 function searchCity(){
-    console.log("test");
 var nameOfCity= document.getElementById('form1').value
 console.log(nameOfCity);
-fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + nameOfCity + '&limit=10&appid=c317fe562c65e62a24be96830a417e95')
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + nameOfCity + '&appid=c317fe562c65e62a24be96830a417e95')
 .then((response) => response.json())
 .then((data) =>{
-    console.log(data)
+   showWeather(data)
 // fetch('api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=c317fe562c65e62a24be96830a417e95')
 });
 
 }
 searchBtn.addEventListener("click", searchCity);
 
+function mainWeather(payload){
+    let template= `
+    <div class="border border-secondary p-2 mt-2">
+                    <div class="fw-bold fs-3 text-left"> ${payload.city} (${payload.date}) <img src="http://openweathermap.org/img/w/${payload.icon}.png"/></div>
+                    <div class="mt-2">Temp: ${payload.temp} F</div>
+                    <div class="mt-2">Wind: ${payload.wind} MPH</div>
+                    <div class="my-2">Humidity: ${payload.humidity} %</div>
+
+                </div>
+                `
+    return template
+}
+function showWeather(data){
+    if(data.cod!=200){
+        return 
+    }
+    document.getElementById('mainsection').innerHTML=''
+    document.getElementById('dailybox').innerHTML=''
+    for(let i=0;data.list.length>i;i+=8){
+        console.log(data.list[i])
+        let payload={
+            city:data.city.name,
+            temp:data.list[i].main.temp,
+            icon:data.list[i].weather[0].icon,
+            wind:data.list[i]. wind.speed,
+            humidity:data.list[i].main.humidity,
+            date:dayjs.unix(data.list[i].dt).format('MM/DD/YYYY')
+        }
+        if(i==0){
+        let mainSection=mainWeather(payload)
+            document.getElementById('mainsection').innerHTML=mainSection
+        
+        }
+        let daily= document.createElement('div')
+        daily.classList.add('border', 'text-light', 'weather-card')
+        daily.innerHTML=dayWeather(payload)
+       
+        document.getElementById('dailybox').appendChild(daily)
+    }
+
+}
+function dayWeather(payload){
+    let template=`
+    <div> Date: ${payload.date}</div>
+    <div> <img src="http://openweathermap.org/img/w/${payload.icon}.png"/></div>
+    <div> Temp: ${payload.temp} F</div>
+    <div> Wind: ${payload.wind} MPH</div>
+    <div> Humidity: ${payload.humidity} %</div>
+  
+    `
+    return template
+}
